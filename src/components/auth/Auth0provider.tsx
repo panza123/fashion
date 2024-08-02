18c1a7a3fa52a2ee3c883fee710d9383d59d8b64
Faruq <olowonlafaruq@gmail.com>
2024-08-02 14:38:21 -0700

    change

--- src/components/auth/Auth0provider.tsx
+++ src/components/auth/Auth0provider.tsx
@@ -1,17 +1,13 @@
-// src/auth/auth0-provider.tsx
 import React from 'react';
 import { Auth0Provider } from '@auth0/auth0-react';
 
 const Auth0ProviderWithHistory: React.FC<{ children: React.ReactNode }> = ({ children }) => {
-
-
   return (
     <Auth0Provider
       domain="dev-11oqec2ojws233w7.us.auth0.com"
       clientId="uRE0TaH9FNZznv1lma5DOektqRuWygwX"
       authorizationParams={{
         redirect_uri: window.location.origin,
-        
       }}
     >
       {children}
