8ee4ed460c3a06741c9d52602c7196c31e4a6aeb
Faruq <olowonlafaruq@gmail.com>
2024-08-02 13:53:03 -0700

    change

--- src/components/auth/LogoutButton.tsx
+++ src/components/auth/LogoutButton.tsx
@@ -1,22 +1,15 @@
-import { useAuth0 } from "@auth0/auth0-react";
+import { useAuth0 } from "@auth0/auth0-react"
 
-export default function LogoutButton() {
-  const { logout } = useAuth0();
-
-  const handleLogout = () => {
-    logout({
-      logoutParams: {
-        returnTo: window.location.origin
-      }
-    });
-  };
-
+export default function LogouButton() {
+    const { logout } = useAuth0()
   return (
     <button
-      onClick={handleLogout}
-      className="text-gray-600 hover:text-orange-600"
+    
+    onClick={()=>logout({logoutParams:{returnTo:window.location.origin}})}
+    
     >
-      Logout
+        Logout
+        
     </button>
-  );
+  )
 }
